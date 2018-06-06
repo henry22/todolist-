486bf29c5953ce7a7367df0e55553cf063a080c9
Henry <henry@Henryde-iMac.local>
2018-06-06 18:25:45 +0800

    Remove sammy js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-var app = $.sammy(function () {
-
-    this.get('#/mytasks.html', function () {
-        //your function
-    });
-    this.get('#/inprogress.html', function () {
-        //your function 
-    });
-    this.get('#/completed.html', function () {
-        //your function
-    });
-});
-app.run();
-
 $('.status li').click(function(e) {
     e.preventDefault();
     $(this).toggleClass('shadow');
