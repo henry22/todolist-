d5acf103b858a3b74bc5484fce6f6bf4a8179085
Henry <henry@Henryde-iMac.local>
2018-06-06 16:44:00 +0800

    Add input

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+var app = $.sammy(function () {
+
+    this.get('#/mytasks.html', function () {
+        //your function
+    });
+    this.get('#/inprogress.html', function () {
+        //your function 
+    });
+    this.get('#/completed.html', function () {
+        //your function
+    });
+});
+app.run();
+
 $('.status li').click(function(e) {
     e.preventDefault();
     $(this).toggleClass('shadow');
-})
+});
