d629bc7b73fa7bbcae3d7a0414135b9bded0d8f1
Henry <henry@Henryde-iMac.local>
2018-07-11 10:12:29 +0800

    Connect to the mongoLab

--- config/index.js
+++ config/index.js
@@ -1,1 +1,7 @@
 var configValues = require('./config');
+
+module.exports = {
+    getDbConnectionString: function() {
+        return 'mogodb//' + configValues.uname + ':' + configValues.pwd + '@ds231951.mlab.com:31951/nodejstodosample';
+    }
+}
